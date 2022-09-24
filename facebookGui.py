9d7440f1ba732db9caa1347563a56a58b69052a2
Noor5567 <83994879+Noor5567@users.noreply.github.com>
2022-09-24 13:31:43 +0300

    new

--- facebookGui.py
+++ facebookGui.py
@@ -36,13 +36,14 @@ if (fireFoxIcon is not None):
     gui.write("https://www.facebook.com/",interval=0.20)
     sleep(2)
     gui.press('enter')
-    sleep(10) ## END TO FIND PLUS IN BROWSER
+    sleep(5) 
+    gui.click(button='left')## END TO FIND PLUS IN BROWSER
 ## START FIND HOME AND EMAIL AND PASSWORD AND LOGIN IMAGES
-FindHomePage=gui.locateAllOnScreen('imgs\\HomaPage.jpg',confidence=0.75)
-FindEmail=gui.locateCenterOnScreen('imgs\\enterEmail.jpg',confidence=0.75)  #x=1175, y=266
-FindPass=gui.locateAllOnScreen('imgs\\enterPassword.jpg',confidence=0.75)  #x=1135, y=341
-FindLogin=gui.locateCenterOnScreen('imgs\\login.jpg',confidence=0.75)
-if(FindHomePage and FindEmail and FindPass and FindLogin is not None):
+ FindHomePage=gui.locateAllOnScreen('imgs\\HomaPage.jpg',confidence=0.75)
+ FindEmail=gui.locateCenterOnScreen('imgs\\enterEmail.jpg',confidence=0.75)  #x=1175, y=266
+ FindPass=gui.locateAllOnScreen('imgs\\enterPassword.jpg',confidence=0.75)  #x=1135, y=341
+ FindLogin=gui.locateCenterOnScreen('imgs\\login.jpg',confidence=0.75)
+ if(FindHomePage and FindEmail and FindPass and FindLogin is not None):
     gui.click(FindEmail[0],FindEmail[1])
     sleep(2)
     gui.write(email,interval=0.20)
